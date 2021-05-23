3295f493a06412aa6fa66feb0adfb52c7afe3a01
Federico Aloi <federico.aloi@gmail.com>
2021-05-23 20:38:52 -0300

    Taller de Docker

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-import Mathjax from 'reveal.js/plugin/math/math.esm.js';
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
 
 import 'reveal.js/dist/reveal.css';
-import 'reveal.js/dist/theme/white.css';
+import 'reveal.js/dist/theme/beige.css';
+import 'reveal.js/plugin/highlight/zenburn.css';
 
 const slide = new Reveal({
-  width: 1024,
-  height: 768,
-  slideNumber: 'c/t',
   hash: true,
-  center: true,
-  plugins: [Markdown, Mathjax],
-  math: {
-    mathjax: 'https://cdn.jsdelivr.net/gh/mathjax/mathjax@2.7.8/MathJax.js',
-    config: 'TeX-AMS_HTML-full',
-  },
-  transitionSpeed: 'fast',
-  mouseWheel: true,
+  plugins: [Markdown, Highlight],
 });
 
 slide.initialize();
